dbc339969d8254e5fcc245cc3f72fe632a781635
christopher <christopher.curran24@gmail.com>
2022-02-14 16:00:56 -0600

    version 1

---
+++ probability_distributions.py
@@ -0,0 +1,17 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+import viz # curriculum viz example code
+
+np.random.seed(123)
+#%%
+# exercise 1
+# A bank found that the average number of cars waiting during the noon 
+# hour at a drive-up window follows a Poisson distribution with a mean of 2 cars. 
+# Make a chart of this distribution and answer these questions concerning the 
+# probability of cars waiting at the drive-up window.
+# 1a. What is the probability that no cars drive up in the noon hour?
+# 1b. What is the probability that 3 or more cars come through the drive through?
+# 1c. How likely is it that the drive through gets at least 1 car?
+
